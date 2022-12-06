a5b2928ce875476e6278496f67a648fb0556cb31
anjalipoudel <sabnom77poudel@gmail.com>
2022-12-07 00:11:23 +0545

    init

---
+++ js/script.js
@@ -0,0 +1,84 @@
+
+flag = 1;
+function sureFunction() {
+    alert("I Love You!");
+}
+function sorryFunction() {
+
+    if (flag == 1) {
+        sorry.style.top = 364 + "px"
+        sorry.style.left = 300 + "px"
+        console.log("button pressed");
+        console.log(flag);
+        flag = 2
+    }
+    else if (flag == 2) {
+        sorry.style.top = 476 + "px"
+        sorry.style.left = 78 + "px"
+        console.log(flag);
+        console.log("button pressed");
+
+        flag = 3
+    }
+    else if (flag == 3) {
+        sorry.style.top = 358 + "px"
+        sorry.style.left = 147 + "px"
+        flag = 4
+    }
+    else if (flag == 4) {
+        sorry.style.top = 490 + "px"
+        sorry.style.left = 134 + "px"
+        flag = 5
+    }
+    else if (flag == 5) {
+        sorry.style.top = 234 + "px"
+        sorry.style.left = 150 + "px"
+        flag = 6
+    }
+    else if (flag == 6) {
+        sorry.style.top = 441 + "px"
+        sorry.style.left = 245 + "px"
+        flag = 7
+    }
+    else if (flag == 7) {
+        sorry.style.top = 345 + "px"
+        sorry.style.left = 158 + "px"
+        flag = 8
+    }
+    else if (flag == 8) {
+        sorry.style.top = 390 + "px"
+        sorry.style.left = 165 + "px"
+        flag = 9
+    }
+    else if (flag == 9) {
+        sorry.style.top = 231 + "px"
+        sorry.style.left = 349 + "px"
+        flag = 10
+    }
+    else if (flag == 10) {
+        sorry.style.top = 123 + "px"
+        sorry.style.left = 289 + "px"
+        flag = 11
+    }
+    else if (flag == 11) {
+        sorry.style.top = 259 + "px"
+        sorry.style.left = 109 + "px"
+        flag = 12
+    }
+    else if (flag == 12) {
+        sorry.style.top = 300 + "px"
+        sorry.style.left = 170 + "px"
+        flag = 13
+    }
+    else if (flag == 13) {
+        sorry.style.top = 267 + "px"
+        sorry.style.left = 120 + "px"
+        flag = 14
+    }
+    else if (flag == 14) {
+        sorry.style.top = 126 + "px"
+        sorry.style.left = 170 + "px"
+        flag = 2
+    }
+
+}
